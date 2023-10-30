135bc35abe7769680b1e2e49a743494468cc0f24
venkuu09 <mailtovenkateshpatel@gmail.com>
2023-10-30 14:39:36 -0500

    Created Board

--- routes/board.js
+++ routes/board.js
@@ -6,7 +6,9 @@ router.get('/', function(req, res, next) {
   let query = req.query
   console.log(`rows ${query.rows}`)
   console.log(`cols ${query.cols}`)
-  res.render('board', { title: 'Board Display', query: query});
+  var rows=Number(query.rows);
+  var cols=Number(query.cols);
+  res.render('board', { title: 'Board Display', query: query });
 });
 
 module.exports = router;
