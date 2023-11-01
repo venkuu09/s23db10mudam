2fd07346a82a2476e8fe22e3015f270bb2b1dbea
venkuu09 <mailtovenkateshpatel@gmail.com>
2023-11-01 00:17:15 -0500

    Final commit

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('vehicles', { title: 'Search Results - Vehicles' });
+  res.render('vehicles', { title: 'Search Results Vehicles' });
 });
 
 module.exports = router;
