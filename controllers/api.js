104557e78c2630043ead26fe669daa7c39af8ed5
venkuu09 <mailtovenkateshpatel@gmail.com>
2023-11-08 16:28:18 -0600

    Added controllers

--- controllers/api.js
+++ controllers/api.js
@@ -2,7 +2,7 @@
 exports.api = function(req, res) {
     res.write('[');
     res.write('{"resource":"vehicles", ');
-    res.write(' "verbs":["GET","PUT","POST","DELETE"] ');
+    res.write(' "verbs":["GET","POST","PUT","DELETE"] ');
     res.write('}');
     res.write(']')
     res.send();
