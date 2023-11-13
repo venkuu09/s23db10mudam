861d1470048f96f0ff8770164e892d786cd00e73
venkuu09 <mailtovenkateshpatel@gmail.com>
2023-11-13 14:35:32 -0600

    Added vehiclescreate.pug

--- controllers/vehicles.js
+++ controllers/vehicles.js
@@ -95,11 +95,25 @@ exports.vehicles_view_all_Page = async function (req, res) {
 };
 
 // Handle a show one view with id specified by query
-    exports.vehicles_view_one_Page = async function(req, res) {
+exports.vehicles_view_one_Page = async function(req, res) {
     console.log("single view for id " + req.query.id)
     try{
     result = await vehicles.findById(req.query.id)
     res.render('vehiclesdetail', { title: 'Vehicles Detail', toShow: result});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+    // Handle building the view for creating a costume.
+    // No body, no in path parameter, no query.
+    // Does not need to be async
+exports.vehicles_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('vehiclescreate', { title: 'vehicles Create'});
     }
     catch(err){
     res.status(500)
