ebb5ae90fc8317c19561ea85a00a6dedee51c452
venkuu09 <mailtovenkateshpatel@gmail.com>
2023-11-23 19:59:33 -0600

    Updated index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var Account = require('../models/account');
 
 router.get('/', function (req, res) {
-  res.render('index', { title: 'Vehicle App', user: req.user });
+  res.render('index', { title: 'Vehicles App', user: req.user });
 });
 router.get('/register', function (req, res) {
   res.render('register', { title: 'Vehicles App Registration' });
@@ -14,36 +14,28 @@ router.post('/register', function (req, res) {
     .then(function (user) {
       if (user != null) {
         console.log("exists " + req.body.username)
-        return res.render('register', {
-          title: 'Registration',
-          message: 'Existing User', account: req.body.username
-        })
+        return res.render('register', {title: 'Registration',
+          message: 'Existing User', account: req.body.username})
       }
       let newAccount = new Account({ username: req.body.username });
       Account.register(newAccount, req.body.password, function (err, user) {
         if (err) {
           console.log("db creation issue " + err)
-          return res.render('register', {
-            title: 'Registration',
-            message: 'access error', account: req.body.username
-          })
+          return res.render('register', {title: 'Registration',
+            message: 'access error', account: req.body.username})
         }
         if (!user) {
-          return res.render('register', {
-            title: 'Registration',
-            message: 'access error', account: req.body.username
-          })
+          return res.render('register', {title: 'Registration',
+            message: 'access error', account: req.body.username})
         }
       })
       console.log('Sucess, redirect');
       res.redirect('/');
-    })
-    .catch(function (err) {
-      return res.render('register', {
-        title: 'Registration',
-        message: 'Registration error', account: req.body.username
-      })
-    })
+  })
+  .catch(function (err) {
+    return res.render('register', {title: 'Registration',
+        message: 'Registration error', account: req.body.username})
+  })
 });
 
 router.get('/login', function (req, res) {
