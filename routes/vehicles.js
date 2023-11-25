2362722401a24ee135ecdf934da549ee6ef4e4ca
venkuu09 <mailtovenkateshpatel@gmail.com>
2023-11-25 16:41:59 -0600

    Added Bonus

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -14,9 +14,9 @@ router.get('/', vehicles_controller.vehicles_view_all_Page );
 /* GET detail vehicles page */
 router.get('/detail', vehicles_controller.vehicles_view_one_Page);
 /* GET create vehicle page */
-router.get('/create', vehicles_controller.vehicles_create_Page);
+router.get('/create', secured, vehicles_controller.vehicles_create_Page);
 /* GET create update page */
 router.get('/update', secured, vehicles_controller.vehicle_update_Page);
 /* GET delete costume page */
-router.get('/delete', vehicles_controller.vehicles_delete_Page);
+router.get('/delete', secured, vehicles_controller.vehicles_delete_Page);
 module.exports = router;
