e81b23e5192d4fcc3deda6cbca5d8aa8ca17efda
venkuu09 <mailtovenkateshpatel@gmail.com>
2023-11-24 20:31:48 -0600

    Updated vehicles.pug

--- models/vehicles.js
+++ models/vehicles.js
@@ -1,7 +1,35 @@
 const mongoose = require("mongoose")
 const vehicleSchema = mongoose.Schema({
-    name :String,
-    mileage : Number,
-    price : Number
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 30,
+        // Custom validator using RegExp
+        validate: {
+            validator: function (v) {
+                return /^[a-zA-Z0-9_]+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid username! Use only letters, numbers, and underscores.`,
+        },
+    },
+    mileage: {
+        type: Number,
+        validate: {
+            validator: function(v) {
+                return v >= 0; // Ensure price is non-negative
+            },
+            message: props => `${props.value} is not a valid price! Price must be a non-negative number.`,
+        },
+    },
+    price: {
+        type: Number,
+        validate: {
+            validator: function(v) {
+                return v >= 0; // Ensure price is non-negative
+            },
+            message: props => `${props.value} is not a valid price! Price must be a non-negative number.`,
+        },
+    },
 })
-module.exports = mongoose.model("vehicles", vehicleSchema)
+module.exports = mongoose.model("Vehicles", vehicleSchema)
--- routes/vehicles.js
+++ routes/vehicles.js
@@ -2,6 +2,13 @@ var express = require('express');
 var vehicles_controller = require('../controllers/vehicles');
 var router = express.Router();
 
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
 /* GET home page. */
 router.get('/', vehicles_controller.vehicles_view_all_Page );
 /* GET detail vehicles page */
@@ -9,7 +16,7 @@ router.get('/detail', vehicles_controller.vehicles_view_one_Page);
 /* GET create vehicle page */
 router.get('/create', vehicles_controller.vehicles_create_Page);
 /* GET create update page */
-router.get('/update', vehicles_controller.vehicle_update_Page);
+router.get('/update', secured, vehicles_controller.vehicle_update_Page);
 /* GET delete costume page */
 router.get('/delete', vehicles_controller.vehicles_delete_Page);
 module.exports = router;
